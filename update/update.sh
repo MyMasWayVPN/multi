a99c625d533060795f976aac09f3ae58cba29481
MyMasWayVPN <107555389+MyMasWayVPN@users.noreply.github.com>
2023-11-10 01:01:50 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -6,12 +6,12 @@ echo -e " [INFO] Downloading Update File"
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /usr/bin/ipsaya "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/ipsaya.sh" && chmod +x /usr/bin/ipsaya
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-ss.sh" && chmod +x /usr/bin/menu-ss
+#wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-ss.sh" && chmod +x /usr/bin/menu-ss
 wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
 wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-vless.sh" && chmod +x /usr/bin/menu-vless
 wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
 wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-#wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
 wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-set.sh" && chmod +x /usr/bin/menu-set
 wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-theme.sh" && chmod +x /usr/bin/menu-theme
 wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/mymaswayvpn/multi/main/update/menu-backup.sh" && chmod +x /usr/bin/menu-backup
